86d552073ade63b97f6fac047fe766fc3dcd3fa4
ovcharenkoav <ovcharenkoav@meta.ua>
2017-04-14 20:44:12 +0300

    document product add.

--- src/Blank/AdminBundle/Controller/DefaultController.php
+++ src/Blank/AdminBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Blank\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Blank\AdminBundle\Document\Product;
 
 class DefaultController extends Controller
 {
@@ -20,6 +21,14 @@ class DefaultController extends Controller
      */
     public function testAction()
     {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        for ($i = 0; $i < 10000; $i++) {
+          $product = new Product();
+          $product->setName("A Foo Bar - $i");
+          $product->setPrice('19.99');
+          $dm->persist($product);
+          $dm->flush();
+        }
         return $this->render('BlankAdminBundle:Default:index.html.twig');
     }
 }
